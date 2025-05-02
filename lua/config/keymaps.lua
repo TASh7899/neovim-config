9b917177dd06a95e81c3ec98b9807be4925f8758
TASh7899 <tash64934@gmail.com>
2025-05-02 21:51:57 +0530

    added keymaps to blink

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,7 @@ vim.api.nvim_set_keymap("n", "-", ":Oil --float<CR>", { noremap = true, silent =
 vim.keymap.set("n", "<space>xx", ":RunCode<CR>", { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap("n", "<C-r>", ":redo<CR>", { noremap = true })
+
+vim.keymap.set("n", "gl", function()
+  vim.diagnostic.open_float()
+end, { desc = "opens diagnostics" })
--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,19 +1,31 @@
 return {
-  "saghen/blink.cmp",
-  dependencies = { "rafamadriz/friendly-snippets" },
-  version = "1.*",
-  opts = {
-    keymap = { preset = "default" },
-    appearance = {
-      nerd_font_variant = "mono",
-    },
-    completion = {
-      documentation = { auto_show = false },
-    },
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-    },
-    fuzzy = { implementation = "prefer_rust_with_warning" },
+  {
+    "saghen/blink.compat",
+    -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
+    version = "*",
+    -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+    lazy = true,
+    -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+    opts = {},
   },
-  opts_extend = { "sources.default" },
+  {
+    "saghen/blink.cmp",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    version = "1.*",
+    opts = {
+      keymap = { preset = "default", ["<enter>"] = { "accept", "fallback" } },
+      appearance = {
+        nerd_font_variant = "mono",
+      },
+      completion = {
+        documentation = { auto_show = true },
+      },
+      signature = { enabled = true },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+      fuzzy = { implementation = "prefer_rust_with_warning" },
+    },
+    opts_extend = { "sources.default" },
+  },
 }
